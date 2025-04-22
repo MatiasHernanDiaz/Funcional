761092369bb985700a19b28922b98b1df9df2751
Diaz Matias Hernan <diaz.matiash@gmail.com>
2025-04-22 20:01:50 -0300

    lo resolvi solito

--- ejercicio2.hs
+++ ejercicio2.hs
@@ -1,16 +1,22 @@
-main :: IO ()
-main = do
-    numero <- readLn
-    case numero of
-        _ | numero < 1 -> putStrLn "es menor que 1 "
-        1 -> putStrLn "uno"
-        2 -> putStrLn "dos"
-        3 -> putStrLn "tres"
-        4 -> putStrLn "cuatro"
-        5 -> putStrLn "cinco"
-        6 -> putStrLn "seis"
-        7 -> putStrLn "siete"
-        8 -> putStrLn "ocho"
-        9 -> putStrLn "nueve"
-        10 -> putStrLn "diez"
-        _ -> putStrLn "No match found"
+fun1 :: Int -> String
+fun1 1 = "uno"
+fun1 2 = "dos"
+fun1 3 = "tres"
+fun1 4 = "cuatro"
+fun1 5 = "cinco"
+fun1 6 = "seis"
+fun1 7 = "siete"
+fun1 8 = "ocho"
+fun1 9 = "nueve"
+fun1 0 = "Debe ingresar un numero natural"
+fun1 x
+  | x < 0 = fst(fun2 x)
+fun1 _ = "Mayor"
+
+
+fun2 :: Int -> (String, Int)
+fun2 x
+  | x < 0 = ("negativo", (-1))
+  | x == 0 = ("cero", 0)
+  | x > 0 = ("positivo", 1)
+  | otherwise = ("No es num", 0)
